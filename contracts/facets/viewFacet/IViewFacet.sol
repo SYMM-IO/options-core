46a85598800e70552541b8e2b3ff9c35b065fdfe
PacificOceanSailor <darjasma@gmail.com>
2025-01-14 23:24:54 +0330

    Add imports

--- contracts/facets/viewFacet/IViewFacet.sol
+++ contracts/facets/viewFacet/IViewFacet.sol
@@ -2,7 +2,11 @@
 // This contract is licensed under the SYMM Core Business Source License 1.1
 // Copyright (c) 2023 Symmetry Labs AG
 // For more information, see https://docs.symm.io/legal-disclaimer/license
-pragma solidity  >= 0.8.18;
+pragma solidity >=0.8.18;
+
+import "../../storages/AccountStorage.sol";
+import "../../storages/IntentStorage.sol";
+import "../../storages/SymbolStorage.sol";
 
 interface IViewFacet{
 
