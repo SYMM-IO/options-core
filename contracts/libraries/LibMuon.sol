6c4a99df571000b1f92bcd74203b26d6db02c9de
Naveed <naveedinno@proton.me>
2025-02-04 12:33:58 +0200

    Extract muon logic from core

--- contracts/libraries/LibMuon.sol
+++ contracts/libraries/LibMuon.sol
@@ -4,43 +4,15 @@
 // For more information, see https://docs.symm.io/legal-disclaimer/license
 pragma solidity >=0.8.18;
 
-import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-import "./LibMuonV04ClientBase.sol";
 import "../storages/SymbolStorage.sol";
 import "../storages/AppStorage.sol";
+import "../interfaces/IMuonOracle.sol";
 
 library LibMuon {
-	using ECDSA for bytes32;
-
 	function getChainId() internal view returns (uint256 id) {
 		assembly {
 			id := chainid()
 		}
-	}
-
-	// CONTEXT for commented out lines
-	// We're utilizing muon signatures for asset pricing and user uPNLs calculations.
-	// Even though these signatures are necessary for full testing of the system, particularly when invoking various methods.
-	// The process of creating automated functional signature for tests has proven to be either impractical or excessively time-consuming. therefore, we've established commenting out the necessary code as a workaround specifically for testing.
-	// Essentially, during testing, we temporarily disable the code sections responsible for validating these signatures. The sections I'm referring to are located within the LibMuon file. Specifically, the body of the 'verifyTSSAndGateway' method is a prime candidate for temporary disablement. In addition, several 'require' statements within other functions of this file, which examine the signatures' expiration status, also need to be temporarily disabled.
-	// However, it is crucial to note that these lines should not be disabled in the production deployed version.
-	// We emphasize this because they are only disabled for testing purposes.
-	function verifyTSSAndGateway(
-		bytes32 hash,
-		SchnorrSign memory sign,
-		bytes memory gatewaySignature,
-		PublicKey memory publicKey,
-		address validGateway
-	) internal pure {
-		// == SignatureCheck( ==
-		bool verified = LibMuonV04ClientBase.muonVerify(uint256(hash), sign, publicKey);
-		require(verified, "LibMuon: TSS not verified");
-
-		hash = hash.toEthSignedMessageHash();
-		address gatewaySignatureSigner = hash.recover(gatewaySignature);
-
-		require(gatewaySignatureSigner == validGateway, "LibMuon: Gateway is not valid");
-		// == ) ==
 	}
 
 	function verifySettlementPriceSig(SettlementPriceSig memory sig) internal view {
@@ -53,18 +25,9 @@ library LibMuon {
 		Oracle storage oracle = symbolLayout.oracles[symbol.oracleId];
 
 		bytes32 hash = keccak256(
-			abi.encodePacked(
-				oracle.muonConfig.muonAppId,
-				sig.reqId,
-				address(this),
-				sig.timestamp,
-				sig.symbolId,
-				sig.settlementPrice,
-				sig.settlementTimestamp,
-				getChainId()
-			)
+			abi.encodePacked(sig.reqId, address(this), sig.timestamp, sig.symbolId, sig.settlementPrice, sig.settlementTimestamp, getChainId())
 		);
-		verifyTSSAndGateway(hash, sig.sigs, sig.gatewaySignature, oracle.muonConfig.muonPublicKey, oracle.muonConfig.validGateway);
+		IMuonOracle(oracle.contractAddress).verifyTSSAndGW(hash, sig.reqId, sig.sigs, sig.gatewaySignature);
 	}
 
 	function verifyLiquidationSig(LiquidationSig memory liquidationSig, address partyB) internal view {
@@ -72,7 +35,6 @@ library LibMuon {
 		Oracle storage oracle = SymbolStorage.layout().oracles[appLayout.partyBConfigs[partyB].oracleId];
 		bytes32 hash = keccak256(
 			abi.encodePacked(
-				oracle.muonConfig.muonAppId,
 				liquidationSig.reqId,
 				liquidationSig.liquidationId,
 				address(this),
@@ -87,12 +49,6 @@ library LibMuon {
 				getChainId()
 			)
 		);
-		LibMuon.verifyTSSAndGateway(
-			hash,
-			liquidationSig.sigs,
-			liquidationSig.gatewaySignature,
-			oracle.muonConfig.muonPublicKey,
-			oracle.muonConfig.validGateway
-		);
+		IMuonOracle(oracle.contractAddress).verifyTSSAndGW(hash, liquidationSig.reqId, liquidationSig.sigs, liquidationSig.gatewaySignature);
 	}
 }
