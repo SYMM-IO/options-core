a761c279d60b19b60cb99992aad8e6f279dc50de
ZigBalthazar <zigbalthazar@gmail.com>
2025-04-23 12:23:44 +0200

    enhance setup and deploy tasks

--- tasks/deployment/deploy.task.ts
+++ tasks/deployment/deploy.task.ts
@@ -1,8 +1,8 @@
 import { task } from "hardhat/config"
+import { Diamond } from "../../types"
 
 task("deploy:deploy", "Deploy, verify and setup facets").setAction(async (_, { run }) => {
-	await run("deploy:diamond", true)
-
+	const diamond = await run("deploy:diamond", true) as Diamond
 	await run("verify:deployment")
-	await run("setup:deployment")
+	await run("setup:deployment", { optionAddress: await diamond.getAddress() })
 })
