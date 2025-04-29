3512173996227274708176589fb80c048f035d0e
Moon knight <moonknight.symmio@gmail.com>
2025-04-29 09:25:11 +0200

    Edit Account facet events

--- contracts/facets/Account/IAccountEvents.sol
+++ contracts/facets/Account/IAccountEvents.sol
@@ -16,15 +16,29 @@ interface IAccountEvents {
 	);
 	event InitiateWithdraw(uint256 id, address user, address to, address collateral, uint256 amount, uint256 newBalance);
 	event CompleteWithdraw(uint256 id);
-	event CancelWithdraw(uint256 id, address user, uint256 newBalance);
+	event CancelWithdraw(uint256 id, address user, address collateral, uint256 amount, uint256 newBalance);
 	event ActivateInstantActionMode(address user, uint256 timestamp);
 	event ProposeToDeactivateInstantActionMode(address user, uint256 timestamp);
 	event DeactivateInstantActionMode(address user, uint256 timestamp);
-	event SyncBalances(address collateral, address partyA, address[] partyBs);
 	event BindToPartyB(address indexed partyA, address indexed partyB);
 	event InitiateUnbindingFromPartyB(address indexed partyA, address indexed partyB, uint256 initiationTime);
 	event CompleteUnbindingFromPartyB(address indexed partyA, address indexed partyB);
 	event CancelUnbindingFromPartyB(address indexed partyA, address indexed partyB);
-	event Allocate(address indexed user, address indexed collateral, address indexed counterParty, uint256 amount, int256 newAllocatedBalance);
-	event Deallocate(address indexed user, address indexed collateral, address indexed counterParty, uint256 amount, int256 newAllocatedBalance);
+	event SyncBalances(address collateral, address partyA, address[] partyBs, uint256 newBalance);
+	event Allocate(
+		address indexed user,
+		address indexed collateral,
+		address indexed counterParty,
+		uint256 amount,
+		uint256 newBalance,
+		int256 newAllocatedBalance
+	);
+	event Deallocate(
+		address indexed user,
+		address indexed collateral,
+		address indexed counterParty,
+		uint256 amount,
+		uint256 newBalance,
+		int256 newAllocatedBalance
+	);
 }
