ff9f551d63ac63f86a50e01fe29b8b80bf314972
Moon knight <moonknight.symmio@gmail.com>
2025-04-29 08:54:25 +0200

    Add UpnlSig

--- contracts/types/WithdrawTypes.sol
+++ contracts/types/WithdrawTypes.sol
@@ -4,6 +4,8 @@
 // For more information, see https://docs.symm.io/legal-disclaimer/license
 pragma solidity >=0.8.19;
 
+import { SchnorrSign } from "./MuonTypes.sol";
+
 struct Withdraw {
 	uint256 id;
 	uint256 amount;
@@ -12,6 +14,17 @@ struct Withdraw {
 	address to;
 	uint256 timestamp;
 	WithdrawStatus status;
+}
+
+struct UpnlSig {
+	bytes reqId; // Unique identifier for the liquidation request
+	uint256 timestamp; // Timestamp when the liquidation signature was created
+	int256 partyAUpnl; // PartyA's unrealized profit and loss at the time
+	int256 partyBUpnl; // PartyB's unrealized profit and loss at the time
+	address collateral; // The address of collateral
+	uint256 collateralPrice; // The price of collateral
+	bytes gatewaySignature; // Signature from the gateway for verification
+	SchnorrSign sigs; // Schnorr signature for additional verification
 }
 
 enum WithdrawStatus {
