2188c039f2d91ef1304715cc33be48e2c34a01d8
ZigBalthazar <zigbalthazar@gmail.com>
2025-05-24 14:36:54 +0200

    fix instant actions issues

--- utils/time.ts
+++ utils/time.ts
@@ -1,5 +1,5 @@
 import { ethers } from "hardhat"
 
-export async function getCurrentLatestBlockTime(): Promise<number> {
+export async function getLatestBlockTime(): Promise<number> {
 	return ethers.provider.getBlock("latest").then(block => block?.timestamp ?? 0)
 }
