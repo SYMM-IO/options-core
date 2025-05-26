f1bf6b95b21cecfc4bafa6506bd1f0394c60d2f1
ZigBalthazar <zigbalthazar@gmail.com>
2025-05-26 13:34:32 +0200

    add test for instantUnlock

--- tests/instant-actions-partyb-open-facet.behavior.ts
+++ tests/instant-actions-partyb-open-facet.behavior.ts
@@ -104,4 +104,79 @@ export function shouldBehaveLikeInstantActionsPartyBOpenFacet(): void {
 			expect(intent.status).to.equal(1) // 1 is LOCKED
 		})
 	})
+
+	describe("instantUnlock", function () {
+		beforeEach(async () => {
+			await context.controlFacet.setMaxTradePerPartyA(3)
+			const latestBlock = await getLatestBlockTime()
+			const request = openIntentRequestBuilder()
+				.partyBsWhiteList([partyB1.getSigner])
+				.affiliate(context.signers.affiliate1)
+				.feeToken(context.collateral)
+				.expirationTimestamp(latestBlock + 120)
+				.deadline(latestBlock + 150)
+				.symbolId(1)
+				.exerciseFee({ cap: e(1), rate: "0" })
+				.marginType(0) // 0 Isolated, 1 Cross
+				.tradeSide(0) // 0 Buy, 1 Sell
+				.quantity(e(100))
+				.price(7)
+				.build()
+
+			// Create and fill open intent
+			await partyA1.sendOpenIntent(request)
+			await partyB1.lockOpenIntent(1)
+		})
+
+		it("Should fail when partyB actions paused", async function () {
+			await context.controlFacet.pausePartyBActions()
+
+			const signedUnlockIntent = signedSimpleActionIntentBuilder().signer(partyB1.address).build()
+			const signedUnlockIntentHash = hashSignedSimpleActionIntent(signedUnlockIntent, "Unlock", context.common.chainId, context.common.diamondAddress)
+			await expect(
+				context.InstantActionsPartyBOpenFacet.connect(partyA1.getSigner).instantUnlock(
+					signedUnlockIntent,
+					await partyB1.sign(signedUnlockIntentHash),
+				),
+			).to.be.revertedWithCustomError(context.instantActionCloseFacet, "PartyBActionsPaused")
+		})
+
+		it("Should fail when thirdParty actions paused", async function () {
+			await context.controlFacet.pauseThirdPartyActions()
+
+			const signedUnlockIntent = signedSimpleActionIntentBuilder().signer(partyB1.address).build()
+			const signedUnlockIntentHash = hashSignedSimpleActionIntent(signedUnlockIntent, "Unlock", context.common.chainId, context.common.diamondAddress)
+			await expect(
+				context.InstantActionsPartyBOpenFacet.connect(partyA1.getSigner).instantUnlock(
+					signedUnlockIntent,
+					await partyB1.sign(signedUnlockIntentHash),
+				),
+			).to.be.revertedWithCustomError(context.instantActionCloseFacet, "ThirdPartyActionsPaused")
+		})
+
+		it("Should fail when partyB signature is invalid", async function () {
+			const signedUnlockIntent = signedSimpleActionIntentBuilder().signer(partyB2.address).build()
+			const signedUnlockIntentHash = hashSignedSimpleActionIntent(signedUnlockIntent, "Unlock", context.common.chainId, context.common.diamondAddress)
+			await expect(
+				context.InstantActionsPartyBOpenFacet.connect(partyA1.getSigner).instantUnlock(
+					signedUnlockIntent,
+					await partyB1.sign(signedUnlockIntentHash),
+				),
+			).to.be.revertedWithCustomError(context.instantActionCloseFacet, "InvalidSignature")
+		})
+
+		it("Should instant cancel close intent successfully", async function () {
+			const signedUnlockIntent = signedSimpleActionIntentBuilder().signer(partyB1.address).build()
+			const signedUnlockIntentHash = hashSignedSimpleActionIntent(signedUnlockIntent, "Unlock", context.common.chainId, context.common.diamondAddress)
+			await expect(
+				await context.InstantActionsPartyBOpenFacet.connect(partyA1.getSigner).instantUnlock(
+					signedUnlockIntent,
+					await partyB1.sign(signedUnlockIntentHash),
+				),
+			).to.not.reverted
+
+			const intent = await context.viewFacet.getOpenIntent(1)
+			expect(intent.status).to.equal(0) // 1 is PENDING
+		})
+	})
 }
