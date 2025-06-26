4d4e520c8de3af8a7d53a77e3a55515935c15b27
Naveed <naveedinno@proton.me>
2025-06-26 08:59:34 +0200

    Add comment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -12,7 +12,7 @@ import { resolve } from "path"
 const dotenvConfigPath: string = process.env.DOTENV_CONFIG_PATH || "./.env"
 dotenvConfig({ path: resolve(__dirname, dotenvConfigPath) })
 
-const privateKey: string | undefined = process.env.PRIVATE_KEY || "0xec81e00837948239d5927bcb2b785675552bc92f1d2607ee91c540ddb56d6796"
+const privateKey: string | undefined = process.env.PRIVATE_KEY || "0xec81e00837948239d5927bcb2b785675552bc92f1d2607ee91c540ddb56d6796" // Dummy private key
 
 const config: HardhatUserConfig = {
 	defaultNetwork: "hardhat",
