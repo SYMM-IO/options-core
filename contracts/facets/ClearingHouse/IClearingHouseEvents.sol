0e0c18131538b504873812ef66391828d7ce6845
timaster <timaster27@proton.me>
2025-06-28 12:36:45 +0200

    Refactor

--- contracts/facets/ClearingHouse/IClearingHouseEvents.sol
+++ contracts/facets/ClearingHouse/IClearingHouseEvents.sol
@@ -27,6 +27,6 @@ interface IClearingHouseEvents {
 		uint256 collateralPrice
 	);
 	event CloseTradesForLiquidation(address operator, uint256[] tradeIds, uint256[] prices);
-    event CancelOpenIntentsForLiquidation(address operator, uint256[] intentId);
+    event CancelOpenIntentsForLiquidation(address operator, uint256[] intentIds);
     event CancelCloseIntentsForLiquidation(address operator, uint256[] intentIds);
 }
