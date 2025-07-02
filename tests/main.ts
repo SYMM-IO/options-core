9397085b6f9d86f716290d0a4030f2dd76ca262a
droplet <tiny_ocean_droplet@gmail.com>
2025-07-02 09:06:51 +0200

    instant layer tests cases

--- tests/main.ts
+++ tests/main.ts
@@ -12,55 +12,48 @@ import { shouldBehaveLikeSettlementFacet } from "./trade-settlement"
 import { shouldBehaveLikeInstantActionCloseFacet } from "./instant-action-close.behavior"
 import { shouldBehaveLikeInstantActionsPartyBOpenFacet } from "./instant-actions-partyb-open-facet.behavior"
 import { shouldBehaveLikeBridgeFacet } from "./bridge-facet.behavior"
+import { shouldBehaveLikeInstantLayer } from "./instant-layer.behavior"
 
 describe(`${name}-v${version}`, () => {
 	if (process.env.TEST_MODE === TestModeEnum.UNIT_TEST) {
-		describe("Facets_Accounts", async function () {
-			shouldBehaveLikeAccountFacet()
-		})
-
-		describe("Facets_PartyAOpenFacet", async function () {
-			shouldBehaveLikePartyAOpenFacet()
-		})
-
-		describe("Facets_PartyBOpenFacet", async function () {
-			shouldBehaveLikePartyBOpenFacet()
-		})
-
-		describe("Facets_PartyACloseFacet", async function () {
-			shouldBehaveLikePartyACloseFacet()
-		})
-
-		describe("Libraries_LibCloseIntent", async function () {
-			shouldBehaveLikeLibCloseIntent()
-		})
-
-		// describe("Facets_InstantActionOpenFacet", async function () {
-		// 	shouldBehaveLikeInstantActionOpenFacet()
+		// describe("Facets_Accounts", async function () {
+		// 	shouldBehaveLikeAccountFacet()
 		// })
 
-		// describe("Facets_InstantActionCloseFacet", async function () {
-		// 	shouldBehaveLikeInstantActionCloseFacet()
+		// describe("Facets_PartyAOpenFacet", async function () {
+		// 	shouldBehaveLikePartyAOpenFacet()
 		// })
 
-		// describe("shouldBehaveLikeInstantActionsPartyBOpenFacet", async function () {
-		// 	shouldBehaveLikeInstantActionsPartyBOpenFacet()
+		// describe("Facets_PartyBOpenFacet", async function () {
+		// 	shouldBehaveLikePartyBOpenFacet()
 		// })
 
-		describe("Facets_PartyBCloseFacet", async function () {
-			shouldBehaveLikePartyBCloseFacet()
-		})
+		// describe("Facets_PartyACloseFacet", async function () {
+		// 	shouldBehaveLikePartyACloseFacet()
+		// })
 
-		describe("Facets_Settlement", async function () {
-			shouldBehaveLikeSettlementFacet()
-		})
+		// describe("Libraries_LibCloseIntent", async function () {
+		// 	shouldBehaveLikeLibCloseIntent()
+		// })
 
-		describe("Facets_ForceActions", async function () {
-			shouldBehaveLikeForceActionFacet()
-		})
+		// describe("Facets_PartyBCloseFacet", async function () {
+		// 	shouldBehaveLikePartyBCloseFacet()
+		// })
 
-		describe("Facet_BridgeFacet", async function () {
-			shouldBehaveLikeBridgeFacet()
+		// describe("Facets_Settlement", async function () {
+		// 	shouldBehaveLikeSettlementFacet()
+		// })
+
+		// describe("Facets_ForceActions", async function () {
+		// 	shouldBehaveLikeForceActionFacet()
+		// })
+
+		// describe("Facet_BridgeFacet", async function () {
+		// 	shouldBehaveLikeBridgeFacet()
+		// })
+
+		describe("Instant Layer", async function () {
+			shouldBehaveLikeInstantLayer()
 		})
 	} else {
 		throw new Error(`Invalid TEST_MODE property. Should be one of: ${Object.keys(TestModeEnum).join(", ")}`)
